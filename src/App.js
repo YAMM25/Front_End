6727d582961c3dcfd00f2f5f69dd02e3019d13f8
YAMM25 <36430354+YAMM25@users.noreply.github.com>
2021-06-03 17:15:33 -0500

    se modificaron los componentes de List y Form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { useContext, useReducer, useEffect, useRef, useState} from 'react'
+import React, { useContext, useReducer, useEffect, useRef, useState, createContext } from 'react'
 
-const HOST_API = "http://localhost:8080/api";
-
+const HOST_API ="http://localhost:8080/api";
 const initialState = {
-  list:[]
+  list:[],
+  item: {}
 };
+const Store = createContext(initialState)
 
 const Form = () =>{
- 
   const formRef = useRef(null);
- 
+  const {dispatch, state: {item}} = useContext(Store);
+  const [state, setState] = useState(item);
+
+
   const onAdd = (event) =>{
     event.preventDefault();
 
@@ -18,7 +21,7 @@ const Form = () =>{
       id: null,
       isComplete: false
     };
-
+  
     fetch(HOST_API+"/todo", {
       method: "POST",
       body: JSON.stringify(request),
@@ -34,12 +37,34 @@ const Form = () =>{
     });
   }
 
-
+    const request ={
+      name: state.name,
+      id: item.id,
+      isComplete: item.isComplete
+    };
+  
+    fetch(HOST_API+"/todo", {
+      method: "PUT",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then((todo)=>{
+      dispatch({ type: "update-item", item: todo});
+      setState({name: ""});
+      formRef.current.reset();
+    });
+  }
+  
   return <form ref={formRef}>
-    <input type="text" name="name" onChange={(event)=>{
+    <input type="text" name="name" defaultValue={item.name} onChange={(event)=>{
       setState({...state, name: event.target.value})
     }}  ></input>
-    <button onClick={onAdd}>Agregar</button>
+    {item.id && <button onClick={onEdit}>Actualizar</button>}
+    {!item.id && <button onClick={onAdd}>Agregar</button> }
+        
   </form>
 }
 
@@ -54,6 +79,19 @@ const List = () =>{
       dispatch({type: "update-list", list})
     })
   }, [state.list.lenght, dispatch]);
+
+  const onDelete = (id) =>{
+    fetch(HOST_API + "/" +id+ "/todo",{
+      method: "DELETE"
+    })
+    .then((list) => {
+      dispatch({ type: "delete-item", id})
+    })
+  };
+
+  const onEdit = (todo) => {
+    dispatch({ type: "edit-item", item: todo})
+  };
 
   return <div>
   <table>
@@ -77,36 +115,51 @@ const List = () =>{
     </tbody>
   </table>
 </div>
-  };
+}
 
-  function reducer(state, action){
-    switch(action.type) {
-      case 'update-list':
-        return {...state, list: action.list}
-      case 'add-item':
-        const newList = state.list;
-        newList.push(action.item);
-        return { ...state, list: newList} 
-      default:
-        return state;   
-    }
+function reducer(state, action){
+  switch(action.type) {
+    case 'update-item':
+      const listUpdateEdit = state.list.map((item) =>{
+        if (item.id === action.item.id){
+          return action.item;
+        }
+        return item;
+      });
+      return {...state, list: listUpdateEdit, item: {}}
+
+    case 'delete-item':
+      const listUpdate = state.list.filter((item) =>{
+        return item.id !== action.id;
+      });
+      return {...state, list: listUpdate}
+    case 'update-list':
+      return {...state, list: action.list}
+    case 'edit-item':
+      return {...state, item: action.item}
+    case 'add-item':
+      const newList = state.list;
+      newList.push(action.item);
+      return { ...state, list: newList} 
+    default:
+      return state;   
   }
+}
 
-  const StoreProvider = ({children}) =>{
+const StoreProvider = ({children}) =>{
 
-    const [state, dispatch] = useReducer(reducer, initialState);
-  
-    return <Store.Provider value={{ state, dispatch}}>
-      {children}
-    </Store.Provider>
-  }
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  function App(){
-    return <StoreProvider> 
-      <Form />
-      <Form />
-    </StoreProvider>
-  }
-  
+  return <Store.Provider value={{ state, dispatch}}>
+    {children}
+  </Store.Provider>
+}
+
+function App(){
+  return <StoreProvider> 
+    <Form />
+    <List />
+  </StoreProvider>
+}
 
 export default App;
